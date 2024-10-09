2cf142086ae48caffbafcbd871285fbc93f664ea
MaryNykoniuk <126586261+MaryNykoniuk@users.noreply.github.com>
2024-10-09 16:31:28 +0300

    Update emotion_recognition_streamlit.py

--- emotion_recognition_streamlit.py
+++ emotion_recognition_streamlit.py
@@ -5,50 +5,55 @@ import numpy as np
 import dlib
 import joblib
 from feature_extraction import find_features
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
+# Завантаження емоцій, детектора облич та інших моделей
 emotions = ['afraid', 'angry', 'disgusted', 'happy', 'neutral', 'sad', 'surprised']
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 emotion_detector = tf.keras.models.load_model("my_model.h5")
 scaler = joblib.load('scaler.save')
 
-class VideoTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.show_landmarks = False
 
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+# Основна логіка програми
+def process_image(cv2_img):
+    image_gray = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+    faces = detector(image_gray, 1)
 
-        image_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = detector(image_gray, 1)
+    for face in faces:
+        l, t, r, b = face.left(), face.top(), face.right(), face.bottom()
+        cv2.rectangle(cv2_img, (l, t), (r, b), (0, 255, 255), 2)
+        landmarks = predictor(image_gray, face)
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(cv2_img, (x, y), 1, (0, 255, 0), -1)
 
-        for face in faces:
-            l, t, r, b = face.left(), face.top(), face.right(), face.bottom()
-            cv2.rectangle(img, (l, t), (r, b), (0, 255, 255), 2)
-            landmarks = predictor(image_gray, face)
-            if self.show_landmarks:
-                for n in range(0, 68):
-                    x = landmarks.part(n).x
-                    y = landmarks.part(n).y
-                    cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
+        features = find_features(landmarks)
+        features_scaled = scaler.transform(features.reshape(1, -1))
+        prediction = emotion_detector.predict(features_scaled.reshape(1, -1))
+        predicted_class = np.argmax(prediction)
+        cv2.putText(cv2_img, emotions[predicted_class], (l, t - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
-            features = find_features(landmarks)
-            features_scaled = scaler.transform(features.reshape(1, -1))
-            prediction = emotion_detector.predict(features_scaled.reshape(1, -1))
-            predicted_class = np.argmax(prediction)
-            cv2.putText(img, emotions[predicted_class], (l, t - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+    return cv2_img
 
-        return img
 
 def main():
     st.set_page_config(page_title="Emotion Recognition App")
     st.title("Emotion Recognition App")
+    enable = st.checkbox("Enable camera")
+    # Отримання зображення з камери
+    img_file_buffer = st.camera_input("Take a picture", disabled=not enable)
 
-    # Додайте чекбокс для вибору користувачем
-    ctx = webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
-    if ctx.video_processor:
-        ctx.video_processor.show_landmarks = st.checkbox("Show landmarks on face")
+    if img_file_buffer is not None:
+        # Читання зображення з буфера за допомогою OpenCV
+        bytes_data = img_file_buffer.getvalue()
+        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+        # Обробка зображення
+        processed_img = process_image(cv2_img)
+
+        # Відображення обробленого зображення
+        st.image(processed_img, channels="BGR")
 
 
 if __name__ == "__main__":
